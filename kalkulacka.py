fff415419a1ea64a591db4d5894d69d7863f6c58
Vym38224 <jardaavymetal@gmail.com>
2022-04-09 14:23:39 +0200

    doma

--- kalkulacka.py
+++ kalkulacka.py
@@ -2,55 +2,8 @@
 import math
 import tkinter as tk
 
+from tkinter import *
 from os.path import basename, splitext
-
-
-class Application(tk.Tk):
-    name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
-
-    def __init__(self):
-        super().__init__(className=self.name)
-        self.title(self.name)
-        self.bind("<Escape>", self.quit)
-        self.lbl = tk.Label(self, text="Kalkulačka")
-        self.lbl.pack()
-        self.btn = tk.Button(self, text="Konec", command=self.quit)
-        self.btn.pack()
-        
-
-
-
-zasobnik = []
-
-
-def operace(token):
-    if token.upper() == "Q":
-        exit()
-    if token.upper() == "PI":
-        zasobnik.append(math.pi)
-    if token == "+":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a + b)
-    if token == "-":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a - b)
-    if token == "*":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a * b)
-    if token == "**":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(a ** b)
-    if token == "sin":
-        a = zasobnik.pop()
-        zasobnik.append(math.sin(a))
-    if token == "cos":
-        a = zasobnik.pop()
-        zasobnik.append(math.cos(a))
 
 
 dva_operandy = {}
@@ -64,54 +17,150 @@ dva_operandy["**"] = lambda a, b: a ** b
 jeden_operand = {}
 jeden_operand["sin"] = math.sin
 jeden_operand["cos"] = math.cos
-jeden_operand["tg"] = math.tan
 jeden_operand["tan"] = math.tan
 
 
-def operace(token):
-    if token.upper() == "Q":
-        exit()
-    if token.upper() == "PI":
-        zasobnik.append(math.pi)
-    if token.upper() == "SW":
-        b = zasobnik.pop()
-        a = zasobnik.pop()
-        zasobnik.append(b)
-        zasobnik.append(a)
-    if token in dva_operandy.keys():
-        if len(zasobnik) >= 2:
-            b = zasobnik.pop()
-            a = zasobnik.pop()
-            zasobnik.append(dva_operandy[token](a, b))
-        else:
-            print("Nemám dost operandů!!!")
-    if token in jeden_operand.keys():
-        if len(zasobnik) >= 1:
-            a = zasobnik.pop()
-            zasobnik.append(jeden_operand[token](a))
-        else:
-            print("Nemám dost operandů!!!")
 
+class Application(tk.Tk):
+    name = basename(splitext(basename(__file__.capitalize()))[0])
+    name = "kalkulacka"
 
-def zpracuj(radek):
-    tokeny = radek.split()
-    for token in tokeny:
+    def __init__(self):
+        super().__init__(className=self.name)
+        self.expression = ""
+        self.zasobnik = []
+        self.equation = StringVar()
+        self.geometry("400x300")
+        self.bind("<Escape>", self.quit)
+        self.lbl = tk.Label(self, text="Kalkulačka")
+        self.lbl.grid(row = 1,column=3)
+        self.btn = tk.Button(self, text = "Konec",bg="pink", command = self.quit, height=1, width=7)
+        self.btn.grid(row = 5, column = 6)
+        self.btnz = tk.Button(self, text="Zapsat",bg="green", command=self.fce, height=1, width=7)
+        self.btnz.grid(row=2, column=6)
+        self.btn1 = Button(self, text=' 1 ', command=lambda: self.press(1), height=1, width=7)
+        self.btn1.grid(row = 2, column = 1)
+        self.btn2 = Button(self, text=' 2 ', command=lambda: self.press(2), height=1, width=7)
+        self.btn2.grid(row= 2, column=2)
+        self.btn3 = Button(self, text=' 3 ', command=lambda: self.press(3), height=1, width=7)
+        self.btn3.grid(row= 2, column=3)
+        self.btn4 = Button(self, text=' 4 ', command=lambda: self.press(4), height=1, width=7)
+        self.btn4.grid(row=3, column=1)
+        self.btn5 = Button(self, text=' 5 ', command=lambda: self.press(5), height=1, width=7)
+        self.btn5.grid(row=3, column=2)
+        self.btn6 = Button(self, text=' 6 ', command=lambda: self.press(6), height=1, width=7)
+        self.btn6.grid(row=3, column=3)
+        self.btn7 = Button(self, text=' 7 ', command=lambda: self.press(7), height=1, width=7)
+        self.btn7.grid(row=4, column=1)
+        self.btn8 = Button(self, text=' 8 ', command=lambda: self.press(8), height=1, width=7)
+        self.btn8.grid(row=4, column=2)
+        self.btn9 = Button(self, text=' 9 ', command=lambda: self.press(9), height=1, width=7)
+        self.btn9.grid(row=4, column=3)
+        self.btn0 = Button(self, text=' 0 ', command=lambda: self.press(0), height=1, width=7)
+        self.btn0.grid(row=5, column=1)
+        self.plus = Button(self, text=' + ', command=lambda: self.press("+"), height=1, width=7)
+        self.plus.grid(row=2, column=4)
+        self.minus = Button(self, text=' - ', command=lambda: self.press("-"), height=1, width=7)
+        self.minus.grid(row=3, column=4)
+        self.multiply = Button(self, text=' * ', command=lambda: self.press("*"), height=1, width=7)
+        self.multiply.grid(row=4, column=4)
+        self.divide = Button(self, text=' / ', command=lambda: self.press("/"), height=1, width=7)
+        self.divide.grid(row=5, column=4)
+        self.radical = Button(self, text=' //', command=lambda: self.press("//"), height=1, width=7)
+        self.radical.grid(row=5, column=5)
+        self.power = Button(self, text=' **', command=lambda: self.press("**"), height=1, width=7)
+        self.power.grid(row=4, column=5)
+        self.decimal = Button(self, text='.', command=lambda: self.press("."), height=1, width=7)
+        self.decimal.grid(row=5, column=2)
+        self.clear = Button(self, text='C', command=self.clear, height=1, width=7)
+        self.clear.grid(row=5, column=3)
+        self.sin = Button(self, text='sin', command=lambda: self.press("sin"), height=1, width=7)
+        self.sin.grid(row=2, column=5)
+        self.cos = Button(self, text='cos', command=lambda: self.press("cos"), height=1, width=7)
+        self.cos.grid(row=3, column=5)
+        self.tan = Button(self, text='tan', command=lambda: self.press("tan"), height=1, width=7)
+        self.tan.grid(row=3, column=6)
+        self.pi = Button(self, text='π', command=lambda: self.press(3,14), height=1, width=7)
+        self.pi.grid(row=4, column=6)
+        
+
+        self.entry = Entry(self, textvariable=self.equation)
+        self.entry.grid(rowspan = 10, columnspan = 7)
+        
+        self.listBox = tk.Listbox(self)
+        self.listBox.grid(rowspan = 11, columnspan = 7)
+
+    def press(self, num):   
+        self.expression = self.expression + str(num)
+        self.equation.set(self.expression)
+
+    def equalpress(self):
         try:
-            zasobnik.append(float(token))
-        except ValueError:
-            operace(token)
+            
+            total = str(eval(self.expression))
+            self.equation.set(total)
+            self.expression = ""
+ 
+        except:
+            self.equation.set(" error ")
+            self.expression = ""
+
+    def clear(self):
+        self.listBox.delete(0,tk.END)
+
+    def fce(self):
+        self.zpracuj(self.entry.get())
+        self.expression = ""
+        self.equation.set("")
 
 
-# čtu vstup
-while True:
-    radek = input(zasobnik.__repr__() + ">>> ")
-    zpracuj(radek)
-
-
-
-    def quit(self, event=None):
+    def quit(self, event = None):
         super().quit()
+
+
+    def operace(self, token, event=None):
+            token = str(self.entry.get())
+            if token.upper() == "Q":
+                exit()
+            if token.upper() == "PI":
+                self.zasobnik.append(math.pi)
+            if token.upper() == "SW":
+                a =  self.listBox.get(tk.END)
+                b =  self.listBox.get(tk.END-1)
+                self.zasobnik.append(b)
+                self.zasobnik.append(a)
+            if token in dva_operandy.keys():
+                if len(self.zasobnik) >= 2:
+                    b = self.zasobnik.pop()
+                    a = self.zasobnik.pop()
+                    self.zasobnik.append(dva_operandy[token](a, b))
+                else:
+                    print("Nemám dost operandů!!!")
+            if token in jeden_operand.keys():
+                if len(self.zasobnik) >= 1:
+                    a = self.zasobnik.pop()
+                    self.zasobnik.append(jeden_operand[token](a))
+                else:
+                    print("Nemám dost operandů!!!")
+            self.listBox.delete(0,tk.END)
+            for token in self.zasobnik:
+                self.listBox.insert(tk.END,token)
+
+
+    def zpracuj(self, token, event=None):
+        try:
+            self.zasobnik.append(float(token))
+            self.listBox.insert(tk.END,token)
+        except ValueError:
+            self.operace(self,token)
+
+
+
+
+    
 
 
 app = Application()
 app.mainloop()
+
+
